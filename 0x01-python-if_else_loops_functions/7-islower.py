87198589d3d4e099dc3c1339ba8177ed710905e5
Joseph Mariga <jsphmariga@gmail.com>
2022-05-31 23:54:03 +0000

    Add file

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-
 def islower(c):
-    numcheck = ord(c)
-    if numcheck >= 97 and numcheck <= 122:
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
