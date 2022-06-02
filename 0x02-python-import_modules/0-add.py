ac1425527e5ea1302b7d679b9be672a4376e082d
Joseph Mariga <jsphmariga@gmail.com>
2022-06-02 23:04:05 +0000

    Add file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-from add_0 import add
 
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-add = add(a,b)
-print(f"{a} + {b} = {add}")
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1 
+    b = 2
+    add = add(a,b)
+    print(f"{a} + {b} = {add}")
 
