daff6a03d24dee1f01bd543d0099341c2166e0a7
Joseph Mariga <jsphmariga@gmail.com>
2022-06-03 00:06:41 +0000

    Add file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
-import sys
 from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
 
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-op = sys.argv[2]
-
-if op == '+':
-    add = add(a, b)
-    print(f"{a} + {b} = {add}")
-elif op == '-':
-    sub = sub(a, b)
-    print(f"{a} - {b} = {sub}")
-elif op == "*":
-    mul = mul(a, b)
-    print(f"{a} * {b} = {mul}")
-elif op == '/':
-    div = div(a, b)
-    print(f"{a} / {b} = {div}")
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
