f0e089c9c2b3f66a94d6b4968c6fc7820e034e06
Joseph Mariga <jsphmariga@gmail.com>
2022-06-03 05:23:23 +0000

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    for i in argv:
+        mysum = int(0)
+        arglen = len(argv) - 1
+        
+        if arglen == 0:
+            print(0)
+        else:
+            for i in range(1,len(argv)):
+                mysum += int(argv[i])
+                        
+    print(f"{mysum}")
