09103dbf43c0225f287fad4017ecf7a23ede3234
Joseph Mariga <jsphmariga@gmail.com>
2022-06-05 14:35:12 +0000

    Find the max [Task 9]

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    max_value = 0
+    if len(my_list) == 0:
+        return None
+    else:
+        for i in range(len(my_list)):
+            if my_list[i] > max_value:
+                max_value = my_list[i]
+    return max_value
