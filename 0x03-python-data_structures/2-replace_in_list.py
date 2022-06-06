36ddc55494698335f77e6ff3b3890fc0d6f77d48
Joseph Mariga <jsphmariga@gmail.com>
2022-06-06 09:15:59 +0000

    Fix pycodestyle [Remove whitespace]

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
@@ -7,4 +8,4 @@ def replace_in_list(my_list, idx, element):
         return my_list
     else:
         my_list[idx] = element
-        return my_list 
+        return my_list
