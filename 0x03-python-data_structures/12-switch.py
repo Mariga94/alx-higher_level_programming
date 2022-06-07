9be41fac099d7ae74923d34d3dc38c8e0d084488
Joseph Mariga <jsphmariga@gmail.com>
2022-06-07 01:04:51 +0000

    Switch

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 a = 89
 b = 10
+temp = b
+b, a = a, temp
+print("a={:d} - b={:d}".format(a, b))
 
-
-def switch_value(a, b):
-    temp = b
-    b = a
-    a = temp
-    print("a={:d} - b={:d}".format(a, b))
-
-
-switch_value(a, b)
