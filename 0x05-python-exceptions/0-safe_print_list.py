3e9607c5b7b1599fe85522313d59aa0944d5ff01
Joseph Mariga <jsphmariga@gmail.com>
2022-06-20 21:00:31 +0000

    Fix indentation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    int = 0
+    count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            int += 1
+            count += 1
         except IndexError:
             break
-        print("")
-        return (int)
+    print("")
+    return (count)
