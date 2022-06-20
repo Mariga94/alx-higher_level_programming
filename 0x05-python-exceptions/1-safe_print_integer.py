3aaafaa1f64166bb8ad75016a006eb8861c28ee8
Joseph Mariga <jsphmariga@gmail.com>
2022-06-20 20:22:03 +0000

    Print list of integers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
-
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
