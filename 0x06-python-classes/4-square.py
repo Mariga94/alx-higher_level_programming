3fa62ffb8592ce549418639017fc03f4d9da1fbd
Joseph Mariga <jsphmariga@gmail.com>
2022-06-22 00:04:12 +0000

    Add correct indentation

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,19 +3,33 @@
 
 
 class Square:
-    """class that defines characteristics of a square"""
+    """defines design a square"""
+
     def __init__(self, size=0):
-        """initialize data"""
-        self.__size = size
-        if not type(size) is int:
+        """initialize data
+        Args:
+        size (int): Size of a new square.
+        """
+    self.__size = size
+    if not type(size) is int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    def area(self):
+        """calculate area of a square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """get the value of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set the value of square"""
+        if not type(value) is int:
             raise TypeError('size must be an integer')
-        if size < 0:
+        elif value < 0:
             raise ValueError('size must be >= 0')
-
-        def area(self):
-            """calculate area of a square"""
-            return self.__size ** 2
-
-        def size(self, value):
-            """sets the value of square"""
-            self.__size = value
+        self.__size = value
