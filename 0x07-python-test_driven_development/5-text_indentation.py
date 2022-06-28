a5cab6a6bde980d5cd91b7e8c3d413d2e1ef01f8
Joseph Mariga <jsphmariga@gmail.com>
2022-06-28 19:03:48 +0000

    Text indentantion

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def text_indentation(text):
+    if not type(text) is str:
+        raise TypeError("text must be a string")
+    else:
+
+        i = 0
+        while i < len(text) and text[i] == ' ':
+            i += 1
+
+        while i < len(text):
+            print(text[i], end="")
+            if text[i] == "\n" or text[i] in ".:?":
+                if text[i] in ".?:":
+                    print("\n")
+                i += 1
+                while i < len(text) and text[i] == ' ':
+                    i += 1
+                    continue
+            i += 1
