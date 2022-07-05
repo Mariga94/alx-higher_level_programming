9a1e3ba0ef7c0756846db926bf3e3bd16dcdcb27
Joseph Mariga <jsphmariga@gmail.com>
2022-07-05 22:34:38 +0000

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """write text to filename"""
+    open(filename 'w', encoding = 'utf-8') as myFile:
+        myFile.write(text)
