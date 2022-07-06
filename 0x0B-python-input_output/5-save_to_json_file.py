1f4dbfda1a2c5c051371be4433690f0857a3475d
Joseph Mariga <jsphmariga@gmail.com>
2022-07-06 01:13:06 +0000

    remove return statement

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using JSON representation"""
     json_str = json.dumps(list(my_obj)) 
     with open(filename, 'w', encoding='utf-8') as myFile:
-        return myFile.write(json_str)
+        myFile.write(json_str)
