3d2c68ea6f5d1961f3695b7b1e405c5cd07e4c5d
Joseph Mariga <jsphmariga@gmail.com>
2022-07-06 02:28:55 +0000

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that returns the dictionary description with simple data structure
+    for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """dictionary representation"""
+    return obj.__dict__
