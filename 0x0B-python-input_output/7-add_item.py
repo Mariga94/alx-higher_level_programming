3536a3a6600813a5eca9e86fdee711b71393e17b
Joseph Mariga <jsphmariga@gmail.com>
2022-07-06 02:20:32 +0000

    Add proper documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Load, add, save"""
+"""Add arguments to a python list and save them to a file"""
 import sys
 
 if __name__ == "__main__":
-    """comm"""
     save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('8-load_from_json_file').load_from_json_file
