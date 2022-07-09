75401451c073f55e52a0424b8d429364a49b9a47
Joseph Mariga <jsphmariga@gmail.com>
2022-07-09 21:00:27 +0000

    Add validators

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -33,7 +33,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if (not isinstance(value, int)):
+        if not type(value) is int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -45,9 +45,9 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if (not isinstance(value, int)):
+        if not type(value) is int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -57,10 +57,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if (not isinstance(value, int)):
+        if not type(value) is int:
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -69,9 +69,9 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if (not isinstance(value, int)):
+        if not type(value) is int:
             raise TypeError("Value must be an integer")
         if value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
