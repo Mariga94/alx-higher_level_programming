df84f6648e1f14374bb661722be4379746e478e5
Joseph Mariga <jsphmariga@gmail.com>
2022-09-06 00:56:01 +0000

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const len = argv.length;
+const len = process.argv.length;
 
 if (len === 2) {
   console.log(0);
@@ -9,8 +8,8 @@ if (len === 2) {
 } else if (len >= 4) {
   const arr = [];
   for (let i = 0; i < len; i++) {
-    if (Number.isInteger(parseInt(argv[i]))) {
-      arr.push(parseInt(argv[i]));
+    if (Number.isInteger(parseInt(process.argv[i]))) {
+      arr.push(parseInt(process.argv[i]));
     }
   }
   const sortDesc = arr.sort((a, b) => b - a);
