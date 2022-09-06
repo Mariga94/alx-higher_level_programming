d8ab4b3621399b0814d465ef135a473378903e68
Joseph Mariga <jsphmariga@gmail.com>
2022-09-06 00:53:04 +0000

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const len = argv.length;
+const len = process.argv.length;
 
 function factorial (num) {
   if (num < 0) {
@@ -17,5 +16,5 @@ function factorial (num) {
 if (len === 2) {
   console.log(1);
 } else if (len === 3) {
-  console.log(factorial(parseInt(argv[2])));
+  console.log(factorial(parseInt(process.argv[2])));
 }
