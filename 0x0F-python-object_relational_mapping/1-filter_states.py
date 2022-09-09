f913bb4a22edbd10cb98d1eed8a853d8ac52ff53
Joseph Mariga <jsphmariga@gmail.com>
2022-09-09 09:14:55 +0000

    Add try catch

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,14 +2,17 @@
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(
-        host="127.0.0.1",
-        port=3306,
-        user=argv[1],
-        password=argv[2],
-        db=argv[3]
-        )
-db.query("""SELECT id, name FROM states WHERE name LIKE "N%"
-        ORDER BY id""")
-result = db.store_result()
-print(result.fetch_row(maxrows=2))
+try:
+    db = MySQLdb.connect(
+            host="127.0.0.1",
+            port=3306,
+            user=argv[1],
+            password=argv[2],
+            db=argv[3]
+            )
+    db.query("""SELECT id, name FROM states WHERE name LIKE "N%"
+            ORDER BY id""")
+    result = db.store_result()
+    print(result.fetch_row(maxrows=2))
+exception MySQLdb.Error as Error:
+    print(Error)
