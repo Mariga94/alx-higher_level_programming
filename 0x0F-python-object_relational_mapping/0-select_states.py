6c07f2aeb3cd8b6a506055c5df3bef6e15d51054
Joseph Mariga <jsphmariga@gmail.com>
2022-09-09 11:39:11 +0000

    Remove Error catching

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,19 +5,9 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    try:
-        db = MySQLdb.connect(
-                host='localhost',
-                port=3306,
-                user=argv[1],
-                passwd=argv[2],
-                db=argv[3])
-        cur = db.cursor()
-        cur.execute("SELECT id, name FROM states ORDER BY id ASC")
-        states = cur.fetchmany(5)
-        for state in states:
-            print(state)
-    except MySQLdb.Error as Error:
-        print(Error)
-    finally:
-        db.close()
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
