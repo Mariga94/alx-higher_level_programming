e191697be26fd3b51c6785294de776e867203486
Joseph Mariga <jsphmariga@gmail.com>
2022-09-22 23:58:59 +0000

    Pipe wc command

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # Takes in a URL, sends a request to the URL
 # and displays the size of the body of the response
-curl -sb -H "Accept: application/json" -r 0-15000  $1 ;
+curl -s "$1" | wc -c ;
