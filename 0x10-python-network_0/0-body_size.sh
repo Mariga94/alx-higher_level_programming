0738bdb056b6e5a6fab3ff6338800508f807ea48
Joseph Mariga <jsphmariga@gmail.com>
2022-09-22 23:35:19 +0000

    Fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # Takes in a URL, sends a request to the URL
 # and displays the size of the body of the response
-curl -s -i -r 0-15000 $1 ;
+curl -sb -H "Accept: application/json" -r 0-15000  $1 ;
