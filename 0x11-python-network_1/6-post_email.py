a953acc100e33218c11706b8ad68f0678922cf49
joseph <jsphmariga@gmail.com>
2022-09-27 22:00:20 +0000

    Fix code

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """
+Takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally display the
+body of the response
 """
 import sys
 import requests
