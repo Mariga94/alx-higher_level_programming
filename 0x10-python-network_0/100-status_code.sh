dc2fa8ce3bd1b7dea632115ec572bde221854076
Joseph Mariga <jsphmariga@gmail.com>
2022-09-27 08:29:26 -0700

    Fix code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # displays only the status code
-<<<<<<< HEAD
 curl -s -o /dev/null -w "%{http_code}" "$1"
-=======
-curl -s -o /dev/null -w "%{http_code}" "$1";
->>>>>>> 29118eef0d495ee4ad23170735d7727e07cf8b0e
