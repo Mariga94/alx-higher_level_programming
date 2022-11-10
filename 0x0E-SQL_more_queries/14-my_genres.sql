85c4f21166b6d79ea156afcc943cc573a17d6dbd
joseph <jsphmariga@gmail.com>
2022-11-10 21:25:37 +0000

    My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,11 @@
 -- Lists all comedy shows in the database hbtn_0d_tvshows.
--- Records are ordered by descending show title.
-SELECT t.`title`
-  FROM `tv_shows` AS t
+-- Records are ordered by 
+SELECT g.`name`
+  FROM `tv_genres` AS g
        INNER JOIN `tv_show_genres` AS s
-       ON t.`id` = s.`show_id`
-
-       INNER JOIN `tv_genres` AS g
        ON g.`id` = s.`genre_id`
-       WHERE g.`name` = "Comedy"
- ORDER BY t.`title`;
+
+       INNER JOIN `tv_shows` AS t
+       ON t.`id` = s.`show_id`
+       WHERE t.`title` = "Dexter"
+ ORDER BY g.`name`;
