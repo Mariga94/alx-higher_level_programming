8bc7d6475b8677dffbe8d6178d6015ed02a58f7a
joseph <jsphmariga@gmail.com>
2022-11-24 17:20:45 +0000

    Fix passwd

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
             host='127.0.0.1',
             port=3306,
             user='root',
-            password='',
+            password='root',
             db='hbtn_0e_4_usa')
 
     cur = db.cursor()
