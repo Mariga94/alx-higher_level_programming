ef29fcd2723a0de47316f94f0b46cab0a2497fb0
joseph <jsphmariga@gmail.com>
2022-11-24 18:16:37 +0000

    Fix code

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,11 +8,9 @@ import MySQLdb
 if __name__ == '__main__':
 
     db = MySQLdb.connect(
-            host='127.0.0.1',
-            port=3306,
-            user='root',
-            password='root',
-            db='hbtn_0e_4_usa')
+            user= sys.argv[1],
+            password= sys.argv[2],
+            db= sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
