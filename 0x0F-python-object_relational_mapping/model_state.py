2071244e573284c5b8e1b8d36b920f35410c6e42
joseph <jsphmariga@gmail.com>
2022-11-25 12:49:34 +0000

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Defines a State model.
+"""
+Defines a State model and inherites from SQLAlchemy Base
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
