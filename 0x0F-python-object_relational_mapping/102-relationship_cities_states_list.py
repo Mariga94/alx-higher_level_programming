c07c50ad715f7f33e61eeacdb3e78bc0fe8c72ed
joseph <jsphmariga@gmail.com>
2022-11-26 13:52:59 +0000

    Add city id

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     session = Session()
 
     for city in session.query(City).order_by(City.id):
-        print("{} -> {}".format(city.name, city.state.name))
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
